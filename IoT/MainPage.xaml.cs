e8ac97d5fa12ff67eccc8887f158703db208683a
Matt Batchelder <mattbatc@gmail.com>
2018-06-19 02:26:59 -0400

    Changed over to a DarkSky system for weather

--- IoT/MainPage.xaml.cs
+++ IoT/MainPage.xaml.cs
@@ -14,7 +14,10 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using IoT.Factory;
 using System.Threading.Tasks;
-
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using DarkSkyApi.Models;
+using DarkSkyApi;
 
 namespace IoT
 {
@@ -25,15 +28,97 @@ namespace IoT
         public MainPage()
         { 
             this.InitializeComponent();
-            DateTime date = DateTime.Today;
-            WelDate.Text = "Hello user, today is a " + (date.DayOfWeek.ToString());
-            getWeather();
+            SetWeather();
+            SetWelcome();
         }
 
-       private async void getWeather()
+       private async void SetWeather()
         {
-            RootObject myWeather = await CurrentWeather.GetWeather();
-            WelWeather.Text = "It is currently " + Math.Round((((myWeather.main.temp - 273) * 1.8) +32))+ " degrees outside";
+            //Getting Weather information
+            var client = new DarkSkyService("31f7fea674b8a860e4ca1094783c554b");
+            Forecast myWeather = await client.GetWeatherDataAsync(42.828422, -73.733340);
+            int temperature =(int) Math.Round( myWeather.Currently.Temperature);
+            RightTemp.Text = temperature.ToString()+"°F";
+
+            //Setting Weather Icon based off conditions
+            Image img = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            Uri uri;
+
+            switch (myWeather.Currently.Icon)
+            {
+                case "clear-day":
+                    uri = new Uri("ms-appx:///Assets/Icons/sunny.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "clear-night":
+                    uri = new Uri("ms-appx:///Assets/Icons/night.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "partly-cloudy-day":
+                    uri = new Uri("ms-appx:///Assets/Icons/partly-cloudy.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "partly-cloudy-night":
+                    uri = new Uri("ms-appx:///Assets/Icons/partly-cloudy-night.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "cloudy":
+                    uri = new Uri("ms-appx:///Assets/Icons/cloudy.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "rain":
+                    uri = new Uri("ms-appx:///Assets/Icons/rain.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "sleet":
+                    uri = new Uri("ms-appx:///Assets/Icons/sleet.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "snow":
+                    uri = new Uri("ms-appx:///Assets/Icons/snow.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "wind":
+                    uri = new Uri("ms-appx:///Assets/Icons/windy.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+
+                case "fog":
+                    uri = new Uri("ms-appx:///Assets/Icons/fog.png");
+                    bitmapImage.UriSource = uri;
+                    WeatherIcon.Source = bitmapImage;
+                    break;
+            }
+
+
+            
+           
         }
+
+        public void SetWelcome()
+        {
+            DateTime date = DateTime.Today;
+            WelDate.Text = "Hello user, today is a " + (date.DayOfWeek.ToString());
+        }
+
+      
     }
 }
